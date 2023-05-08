e251446d8bb4cf543679cdbec5b0b89ac47a562c
sanjeevnara7 <sanjeev.nara@gmail.com>
2023-05-08 19:45:20 +0000

    Add config for our dataset, update Yolov8 training script

--- models/Yolov8Trainer.py
+++ models/Yolov8Trainer.py
@@ -1,12 +1,15 @@
 '''
-Training Script for Yolov8. Yolov8 configs are added to custom config file configs/yolo/hyp-yolov8.yaml for overriding defaults.
+Simple Training Script for Yolov8. Yolov8 configs are added to custom config file configs/yolo/hyp-yolov8.yaml for overriding defaults.
 '''
 import torch
 from ultralytics import YOLO
 
 def main():
-    # Load Yolo model with custom config file
-    model = YOLO('configs/yolo/yolov8l.yaml').load('yolov8l.pt')  # build from YAML and transfer weights
+    CONFIG_FILE_PATH = 'configs/yolo/hyp-yolov8.yaml'
+    # Load Yolo model pretrained on SoccerNet Tracking Frames
+    model = YOLO('runs/detect/yolov8l_trackletframes-2.5k_fixed_res_1080/weights/best.pt')
+    
+    #model = YOLO('configs/yolo/yolov8l.yaml').load('yolov8l.pt') #Load Yolov8 with MS COCO weights
     
     # Get Device
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -18,10 +21,11 @@ def main():
     print('*'*60)
     
     # Train the model
-    model.train(cfg="configs/yolo/hyp-yolov8.yaml")
-    
+    model.train(cfg=CONFIG_FILE_PATH)
+    print('*'*60)
+    print('*'*60)
     # Check results on validation
-    results = model.val()
+    results = model.val(imgsz=1080)
     print(results)
 
 if __name__ == '__main__':
